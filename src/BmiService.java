27a230f28371d45976d7684d36d6dabe982647ab
Artem Blokhin <rtmdragee@gmail.com>
2024-03-22 00:33:17 +0400

    Reformat code

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,4 +1,4 @@
-public class BmiService {
+public class bmiService {
 
     public double calculate(double weight, double height) {
 
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        BmiService service = new BmiService();
+        bmiService service = new bmiService();
         double height = 1.64;
         double weight = 70;
         double BMI = service.calculate(weight, height);
